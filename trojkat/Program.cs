7795a9aac860ee0090e13559d0a1b1dccc78f9ff
kamil <kamil@DESKTOP-MJ9DEK1>
2023-11-26 09:10:49 +0100

    Dodaj pliki projektów.

---
+++ trojkat/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Przyprostokątne
+        double a = 3.5;
+        double b = 6;
+
+        // Obliczenia zgodnie z twierdzeniem Pitagorasa
+        double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        // Wyświetlenie wyniku w konsoli
+        Console.WriteLine($"Długość przeciwprostokątnej trójkąta prostokątnego o przyprostokątnych {a} i {b} wynosi: {c}");
+    }
+}
